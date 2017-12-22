3d2047b5338266d70233fe25c015ea09b3ec0fc8
alanfirmiano <alanfirmiano18@gmail.com>
2017-12-22 12:05:12 -0300

    Mostrando o numero de cotações

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -14,6 +14,18 @@ export class ContactPage{
   public list_cotacoes = new Array<any>();
   public idt:number;
   public name:string;
+  public data:string;
+  dateReviver(value:any) {
+    var a;
+    if (typeof value === 'string') {
+      a = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/.exec(value);
+      if (a) {
+        return new Date(Date.UTC(+a[1], +a[2] - 1, +a[3], +a[4],
+          +a[5], +a[6]));
+      }
+    }
+    return value;
+  };
   constructor(
     public navCtrl : NavController,
     private navParams :NavParams,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,14 +15,38 @@ import {objectAssign} from "@ionic/app-scripts";
 })
 export class HomePage {
   public list_acoes = new Array<any>();
+
   public idt:number;
   public name:string;
+
+  searchQuery: string = '';
+  list = new Array<any>();
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private acoesProvider:AcoesProvider
   ){
+    this.initializeItems();
+  }
 
+  initializeItems() {
+      this.list = this.list_acoes;
+  }
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.list_acoes = this.list_acoes.filter(
+        (item) => {
+        return (item.code.toLowerCase().indexOf(val.toLowerCase()) != -1);
+      })
+    }
   }
   ionViewDidLoad() {
     this.acoesProvider.getListAcoes().subscribe(
